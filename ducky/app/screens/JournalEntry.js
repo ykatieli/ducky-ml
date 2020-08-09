cd5ffc44d13c26757c362977504c5be6f6b5c541
Haley <haleyalee@gmail.com>
2020-08-09 14:42:55 -0400

    fixed keyboard errors, dock component, time bug, and back button!

--- ducky/app/screens/JournalEntry.js
+++ ducky/app/screens/JournalEntry.js
@@ -1,12 +1,13 @@
 // JournalEntry.js
 
 import React, { Component, useState, useEffect, useRef } from 'react';
-import { View, Text, TextInput, Keyboard, TouchableWithoutFeedback, Button } from 'react-native';
+import { View, Text, TextInput, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome, Ionicons, Entypo, MaterialIcons } from '@expo/vector-icons';
 
 import styles from '../Styles';
 import Dock from '../components/Dock';
+import KeyboardShift from '../components/KeyboardShift';
 import firebase from 'firebase';
 
 var config = {
@@ -46,121 +47,121 @@ export class JournalEntry extends Component {
     this.handleTags = this.handleTags.bind(this);
   }
 
-  componentWillMount() {
+  // componentWillMount() {
     
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
+  //   var date = new Date().getDate();
+  //   var month = new Date().getMonth() + 1;
+  //   var year = new Date().getFullYear();
 
-    var fullMonth;
+  //   var fullMonth;
 
-    switch (month) {
-      case 1:
-        fullMonth = 'January';
-        break;
-      case 2:
-        fullMonth = 'February';
-        break;
-      case 3:
-        fullMonth = 'March';
-        break;
-      case 4:
-        fullMonth = 'April';
-        break;
-      case 5:
-        fullMonth = 'May';
-        break;
-      case 6:
-        fullMonth = 'June';
-        break;
-      case 7:
-        fullMonth = 'July';
-        break;
-      case 8:
-        fullMonth = 'August';
-        break;
-      case 9:
-        fullMonth = 'September';
-        break;
-      case 10:
-        fullMonth = 'October';
-        break;
-      case 11:
-        fullMonth = 'November';
-        break;
-      case 12:
-        fullMonth = 'December';
-        break;
-      default:
-        fullMonth = 'Error';
-    }
+  //   switch (month) {
+  //     case 1:
+  //       fullMonth = 'January';
+  //       break;
+  //     case 2:
+  //       fullMonth = 'February';
+  //       break;
+  //     case 3:
+  //       fullMonth = 'March';
+  //       break;
+  //     case 4:
+  //       fullMonth = 'April';
+  //       break;
+  //     case 5:
+  //       fullMonth = 'May';
+  //       break;
+  //     case 6:
+  //       fullMonth = 'June';
+  //       break;
+  //     case 7:
+  //       fullMonth = 'July';
+  //       break;
+  //     case 8:
+  //       fullMonth = 'August';
+  //       break;
+  //     case 9:
+  //       fullMonth = 'September';
+  //       break;
+  //     case 10:
+  //       fullMonth = 'October';
+  //       break;
+  //     case 11:
+  //       fullMonth = 'November';
+  //       break;
+  //     case 12:
+  //       fullMonth = 'December';
+  //       break;
+  //     default:
+  //       fullMonth = 'Error';
+  //   }
 
   
-    // set date state here
-    this.setState({ date: fullMonth + ' ' + date + ', ' + year });
+  //   // set date state here
+  //   this.setState({ date: fullMonth + ' ' + date + ', ' + year });
 
-    var hours = new Date().getHours();
-    var min = new Date().getMinutes();
+  //   var hours = new Date().getHours();
+  //   var min = new Date().getMinutes();
     
-    min = (min<10?'0':'') + min;
+  //   min = (min<10?'0':'') + min;
 
-    if (hours < 12 || hours === 24) {
-      if (hours === 24) hours = 12;
-      this.setState({ time: hours + ':' + min + ' am' });
-    }
-    else {
-      hours = hours - 12;
-      this.setState({ time: hours + ':' + min + ' pm' });
-    }
+  //   if (hours <= 12 || hours === 24) {
+  //     if (hours === 24) hours = 12;
+  //     this.setState({ time: hours + ':' + min + ' am' });
+  //   }
+  //   else {
+  //     hours = (hours == 12) ? hours : hours - 12;
+  //     this.setState({ time: hours + ':' + min + ' pm' });
+  //   }
 
-    //set entry state here
-    // CHANGE: expEntry to text put into the journal entry
-    this.setState({entry: expEntry});
+  //   //set entry state here
+  //   // CHANGE: expEntry to text put into the journal entry
+  //   this.setState({entry: expEntry});
 
-    // CHANGE: hardcoded now
-    this.setState({entryTitle: "New Entry"});
+  //   // CHANGE: hardcoded now
+  //   this.setState({entryTitle: "New Entry"});
 
     
 
 
-    if (!firebase.apps.length){
-      firebase.initializeApp(config);
-    }
+  //   if (!firebase.apps.length){
+  //     firebase.initializeApp(config);
+  //   }
 
-    // To select data from firebase every time data has changed !
-    // Utilize the current database to fill in journal information
-    firebase.database().ref('users/').on('value', (data) => {
-        console.log(data.toJSON());
-    })
+  //   // To select data from firebase every time data has changed !
+  //   // Utilize the current database to fill in journal information
+  //   firebase.database().ref('users/').on('value', (data) => {
+  //       console.log(data.toJSON());
+  //   })
 
-    // To Await 5 seconds to insert a new user
-    // CHANGE: expUser to specific user
-    setTimeout(() => {
-        firebase.database().ref('users/expUser/' + Date.now().toString()).set(
-            {
-                entryTitle: this.state.entryTitle,
-                entry: this.state.entry,
-                time: this.state.time,
-                date: this.state.date,
-                tags: this.state.tags
-            }
-        ).then(() => {
-            console.log('INSERTED !');
-        }).catch((error) => {
-            console.log(error);
-        });
-    }, 10000);
-    // change this depending on how quickly you want it processed
+  //   // To Await 5 seconds to insert a new user
+  //   // CHANGE: expUser to specific user
+  //   setTimeout(() => {
+  //       firebase.database().ref('users/expUser/' + Date.now().toString()).set(
+  //           {
+  //               entryTitle: this.state.entryTitle,
+  //               entry: this.state.entry,
+  //               time: this.state.time,
+  //               date: this.state.date,
+  //               tags: this.state.tags
+  //           }
+  //       ).then(() => {
+  //           console.log('INSERTED !');
+  //       }).catch((error) => {
+  //           console.log(error);
+  //       });
+  //   }, 10000);
+  //   // change this depending on how quickly you want it processed
 
-    // // To Update a user
-    // firebase.database().ref('users/004').update({
-    //     name: 'Pheng Sengvuthy'
-    // });
+  //   // // To Update a user
+  //   // firebase.database().ref('users/004').update({
+  //   //     name: 'Pheng Sengvuthy'
+  //   // });
 
-    // // To Remove a user
-    // firebase.database().ref('users/004').remove();
+  //   // // To Remove a user
+  //   // firebase.database().ref('users/004').remove();
 
-  }
+  // }
 
   componentDidMount() {
 
@@ -225,7 +226,7 @@ export class JournalEntry extends Component {
       this.setState({ time: hours + ':' + min + ' am' });
     }
     else {
-      hours = hours - 12;
+      hours = (hours == 12) ? hours : hours - 12;
       this.setState({ time: hours + ':' + min + ' pm' });
     }
   }
@@ -244,105 +245,96 @@ export class JournalEntry extends Component {
 
   render() {
     return (
-      <LinearGradient colors={['#6B8DB2', '#7998B9']} style={styles.container}>
 
-        {/* Back Button */}
-        <View style={styles.top_left}>
-          <Ionicons name="ios-arrow-back" size={25} color="#ffffff" />
-        </View>
-
-        {/* Entry Information */}
-        <View style={{ paddingLeft: 30, paddingRight: 30 }}>
-          <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
-            <Text style={styles.white_15}>{this.state.date}</Text>
-            <Text style={styles.white_15}>{this.state.time}</Text>
+      <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss() }>
+        <LinearGradient colors={['#6B8DB2', '#7998B9']} style={styles.container}>
+          
+          {/* Back Button */}
+          <View style={styles.top_left}>
+            <Ionicons name="ios-arrow-back" size={25} color="#ffffff" onPress={ () => this.props.navigation.goBack() }/>
           </View>
 
-          {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}> */}
-            <View>
-              <TextInput style = {styles.white_25}
-                underlineColorAndroid = "transparent"
-                placeholder = "placeholder title"
-                placeholderTextColor = "#fbfbfb"
-                autoCapitalize = "none"
-                onChangeText = {this.handleEntryTitle}
-                
-              />
+          {/* Entry Information */}
+          <View style={{ paddingLeft: 30, paddingRight: 30 }}>
+            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }}>
+              <Text style={styles.white_15}>{this.state.date}</Text>
+              <Text style={styles.white_15}>{this.state.time}</Text>
             </View>
-          {/* </TouchableWithoutFeedback> */}
-        </View>
+              <View>
+                <TextInput style = {styles.white_25}
+                  underlineColorAndroid = "transparent"
+                  placeholder = "placeholder title"
+                  placeholderTextColor = "#fbfbfb"
+                  autoCapitalize = "none"
+                  onChangeText = {this.handleEntryTitle}
+                />
+              </View>
+          </View>
 
-        {/* Tools */}
-        <View style={{display: 'flex', flexDirection: 'row', paddingLeft: 30, paddingRight: 30, paddingTop: 20}}> 
-          <MaterialIcons name="format-align-left" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-          <MaterialIcons name="format-align-center" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-          <MaterialIcons name="format-align-right" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-          <MaterialIcons name="format-bold" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-          <MaterialIcons name="format-italic" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-          <MaterialIcons name="format-underlined" size={25} color="#fbfbfb" style={{marginRight: 15}} />
-        </View>
+          {/* Tools */}
+          <View style={{display: 'flex', flexDirection: 'row', paddingLeft: 30, paddingRight: 30, paddingTop: 20}}> 
+            <MaterialIcons name="format-align-left" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+            <MaterialIcons name="format-align-center" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+            <MaterialIcons name="format-align-right" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+            <MaterialIcons name="format-bold" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+            <MaterialIcons name="format-italic" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+            <MaterialIcons name="format-underlined" size={25} color="#fbfbfb" style={{marginRight: 15}} />
+          </View>
 
-        {/* Text Space */}
-        <View style={styles.corner_card} >
-          {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss()} accessible={false}> */}
-            <View style={{padding: 30}}>
-              <TextInput style = {styles.blue_18}
-                underlineColorAndroid = "transparent"
-                placeholder = "Begin entry here..."
-                placeholderTextColor = "#718399"
-                autoCapitalize = "none"
-                multiline={true}
-                onChangeText = {this.handleEntry}
-              />
-            </View>
-          {/* </TouchableWithoutFeedback> */}
+          {/* Text Space */}
+          <View style={styles.corner_card} >
+              <View style={{padding: 30}}>
+                <TextInput style = {styles.blue_18}
+                  underlineColorAndroid = "transparent"
+                  placeholder = "Begin entry here..."
+                  placeholderTextColor = "#718399"
+                  autoCapitalize = "none"
+                  multiline={true}
+                  onChangeText = {this.handleEntry}
+                />   
+              </View>
 
-          {/* Bottom Bar */}
-          <View style={styles.bottom_bar}>
-            {/* Tags */}
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={styles.blue_20}>Tags:</Text>
-              {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss()} accessible={false}> */}
-                <View style={{marginLeft: 10}}>
-                  <TextInput style = {styles.blue_15}
-                    placeholder = "#"
-                    placeholderTextColor = "#718399"
-                    autoCapitalize = "none"
-                    onChangeText = {this.handleTags}
-                  />
+            {/* Bottom Bar */}
+            <KeyboardShift>
+              { () => (
+            <View style={styles.bottom_bar}>
+              {/* Tags */}
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={styles.blue_20}>Tags:</Text>
+                  <View style={{marginLeft: 10}}>
+                    <TextInput style = {styles.blue_15}
+                      placeholder = "#"
+                      placeholderTextColor = "#718399"
+                      autoCapitalize = "none"
+                      onChangeText = {this.handleTags}
+                    />
+                  </View>
+              </View>
+              {/* Mood and Create Entry */}
+              <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingRight: 20 }}>
+                <Text style={styles.blue_20}>Mood:</Text>
+                <View style={{ flexDirection: 'row', width: 150, justifyContent: 'space-evenly' }}>
+                  <MaterialIcons name="sentiment-satisfied" size={25} color="#f9e067" />
+                  <MaterialIcons name="sentiment-neutral" size={25} color="#f9e067" />
+                  <MaterialIcons name="sentiment-dissatisfied" size={25} color="#f9e067" />
                 </View>
-              {/* </TouchableWithoutFeedback> */}
+                {/* Create Entry Button */}
+                <View style={styles.button_entry} onPress={() => null}>
+                  <Text style={styles.white_20}>Create Entry</Text>
+
+                  {/* Change this so that when we press this button, we update all states, then call the database update */}
+                </View>
+              </View> 
             </View>
-            {/* Mood and Create Entry */}
-            <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingRight: 20 }}>
-              <Text style={styles.blue_20}>Mood:</Text>
-              <View style={{ flexDirection: 'row', width: 150, justifyContent: 'space-evenly' }}>
-                <MaterialIcons name="sentiment-satisfied" size={25} color="#f9e067" />
-                <MaterialIcons name="sentiment-neutral" size={25} color="#f9e067" />
-                <MaterialIcons name="sentiment-dissatisfied" size={25} color="#f9e067" />
-              </View>
-              {/* Create Entry Button */}
-              <View style={styles.button_entry}>
-                <Text style={styles.white_20}>Create Entry</Text>
+              )}
+            </KeyboardShift>
+          </View>          
 
-                {/* Change this so that when we press this button, we update all states, then call the database update */}
-              </View>
-            </View>
-          </View>
-        </View>
+          {/* Dock */}
+          <Dock navigation={this.props.navigation}/>
 
-        {/* Dock */}
-        <View style={styles.dock_container}>
-          <View style={styles.dock}>
-            <Feather name="home" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HomeScreen')} />
-            <MaterialCommunityIcons name="calendar-check-outline" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HabitTrackerScreen')} />
-            <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} />
-            <FontAwesome name="pencil-square-o" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('JournalScreen')} />
-            <AntDesign name="areachart" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('AnalyticsScreen')} />
-          </View>
-        </View>
-
-      </LinearGradient>
+        </LinearGradient>
+      </TouchableWithoutFeedback>
     )
   }
 }
--- ducky/app/screens/Profile.js
+++ ducky/app/screens/Profile.js
@@ -6,6 +6,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 
 import styles from '../Styles';
+import Dock from '../components/Dock';
 
 export class Profile extends Component {
   render() {
@@ -49,15 +50,7 @@ export class Profile extends Component {
         </View>
 
         {/* Dock */}
-        <View style={styles.dock_container}>
-          <View style={styles.dock}>
-            <Feather name="home" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HomeScreen')} />
-            <MaterialCommunityIcons name="calendar-check-outline" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HabitTrackerScreen')} />
-            <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} />
-            <FontAwesome name="pencil-square-o" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('JournalScreen')} />
-            <AntDesign name="areachart" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('AnalyticsScreen')} />
-          </View>
-        </View>
+        <Dock navigation={this.props.navigation}/>
 
       </View>
     )
