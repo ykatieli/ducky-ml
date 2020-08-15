d216150e00d110e10772cbe17308eb15059d62f5
Haley <haleyalee@gmail.com>
2020-08-15 15:59:42 -0400

    added ducky logo

--- ducky/app/components/Dock.js
+++ ducky/app/components/Dock.js
@@ -1,11 +1,12 @@
 // Dock.js
 
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
 
 import styles from '../Styles';
+
 
 export class Dock extends Component {
   render() {
@@ -14,7 +15,8 @@ export class Dock extends Component {
         <View style={styles.dock}>
           <Feather name="home" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HomeScreen')} />
           <MaterialCommunityIcons name="calendar-check-outline" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HabitTrackerScreen')} />
-          <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} />
+          {/* <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} /> */}
+          <Image style={styles.dock_icon} source={require('../../assets/logo.png')}></Image>
           <FontAwesome name="pencil-square-o" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('JournalScreen')} />
           <AntDesign name="areachart" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('AnalyticsScreen')} />
         </View>
