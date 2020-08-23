dd47ed27d4dca2db1074ef0a9db6c3d3552bfa25
ykatieli <ykatieli@gmail.com>
2020-08-23 17:56:57 -0400

    editing Profile docks

--- ducky/app/screens/AccountInfo.js
+++ ducky/app/screens/AccountInfo.js
@@ -4,6 +4,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome, Ionicons } from '@expo/vector-icons';
 
 import styles from '../Styles';
+import Dock from '../components/Dock';
 
 export class AccountInfo extends Component {
   render() {
@@ -36,15 +37,7 @@ export class AccountInfo extends Component {
           </LinearGradient>
 
         {/* Dock */}
-        <View style={styles.dock_container}>
-          <View style={styles.dock}>
-            <Feather name="home" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HomeScreen')} />
-            <MaterialCommunityIcons name="calendar-check-outline" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HabitTrackerScreen')} />
-            <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} />
-            <FontAwesome name="pencil-square-o" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('JournalScreen')} />
-            <AntDesign name="areachart" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('AnalyticsScreen')} />
-          </View>
-        </View>
+        <Dock navigation={this.props.navigation}/>
 
       </LinearGradient>
     )
--- ducky/app/screens/Tracker.js
+++ ducky/app/screens/Tracker.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome, Ionicons } from '@expo/vector-icons';
+import { AntDesign, Feather, MaterialCommunityIcons, FontAwesome,  Ionicons } from '@expo/vector-icons';
 
 import styles from '../Styles';
+import Dock from '../components/Dock';
 
 export class Tracker extends Component {
   render() {
@@ -14,9 +15,6 @@ export class Tracker extends Component {
         <View style={styles.top_left}>
             <Ionicons name="ios-arrow-back" size={25} color="#ffffff" onPress={ () => this.props.navigation.goBack() }/>
         </View>
-
-        {/* Profile icon */}
-        {/* <MaterialCommunityIcons name="account-circle" size={50} color="#fff" style={styles.top_right} onPress={() => this.props.navigation.navigate('ProfileScreen')}/> */}
 
         {/* Introduction */}
         <View style={{alignItems: 'center', paddingTop: 0}}>
@@ -33,15 +31,7 @@ export class Tracker extends Component {
           </LinearGradient>
 
         {/* Dock */}
-        <View style={styles.dock_container}>
-          <View style={styles.dock}>
-            <Feather name="home" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HomeScreen')} />
-            <MaterialCommunityIcons name="calendar-check-outline" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('HabitTrackerScreen')} />
-            <MaterialCommunityIcons name="duck" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('DuckyScreen')} />
-            <FontAwesome name="pencil-square-o" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('JournalScreen')} />
-            <AntDesign name="areachart" size={50} color="#FFFFFF" onPress={() => this.props.navigation.navigate('AnalyticsScreen')} />
-          </View>
-        </View>
+        <Dock navigation={this.props.navigation}/>
 
       </LinearGradient>
     )
